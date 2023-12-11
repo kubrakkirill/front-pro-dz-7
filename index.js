e7dff871236979b447d23da9999db8b659cf9613
kubrakk <105593907+kubrakkirill@users.noreply.github.com>
2023-12-11 23:30:42 +0100

    fixed

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-const array = [1, '2', 5, 3, '5', 4, 5, 6, '7'];
+const array = [1, '2', 5, 5, 3, '5', 4, 5, 6, 5, 5, 5, '7'];
 const removeElement = (arr, item) =>{
-    for(let i = 0; i <= arr.length; i++){
+    for(let i = 0; i < arr.length; i++){
         if(arr[i] === item){
             arr.splice(i, 1)
+            i--
         }
     }
 }
